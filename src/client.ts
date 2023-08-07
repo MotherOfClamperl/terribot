ebf8f0e2112a56db1dfe020fb838661f974213ed
motherofclamperl <motherofclamperl@gmail.com>
2023-08-07 00:46:44 -0700

    remove unused / 'disallowed' intents

--- src/client.ts
+++ src/client.ts
@@ -1,21 +1,20 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-import { registerEvents } from './utils/index.js';
-import Events from './events/index.js';
-import Keys from './keys.js';
+import { Client, GatewayIntentBits } from "discord.js";
+import { registerEvents } from "./utils/index.js";
+import Events from "./events/index.js";
+import Keys from "./keys.js";
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		// GatewayIntentBits.GuildMembers,
+		// GatewayIntentBits.GuildMessages,
+		// GatewayIntentBits.MessageContent,
+	],
 });
 
 registerEvents(client, Events);
 
-client.login(Keys.clientToken)
-  .catch((err) => {
-    console.error('[Login Error]', err);
-    process.exit(1);
-  });
+client.login(Keys.clientToken).catch((err) => {
+	console.error("[Login Error]", err);
+	process.exit(1);
+});
