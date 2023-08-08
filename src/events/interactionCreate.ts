bb24d5eef34787b1dd34978d466b30eb988341bd
motherofclamperl <motherofclamperl@gmail.com>
2023-08-07 20:15:40 -0700

    add /spinoffs command (3)

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,11 +1,38 @@
 import { event, Events } from "../utils/index.js";
 
+const liveTags: string[] = [];
+
+const formatTag = (tag: string) => tag.replace(/_/g, "`_`"); // _ in @ in discord message links []() are auto-removed
+async function getLiveTags() {
+	try {
+		const response = await fetch(
+			"https://terriverse.vercel.app/api/who-live/spinoffs"
+		);
+		const data = await response.json();
+		liveTags.splice(0, liveTags.length); // clear array
+		for (let tag in data.tagInfo)
+			if (data.tagInfo[tag]) liveTags.push("@" + tag);
+	} catch (error) {
+		console.error("Error fetching data:", error);
+	}
+}
+
 export default event(
 	Events.InteractionCreate,
 	({ log, client }, interaction) => {
 		if (!interaction.isChatInputCommand()) return;
 		if (interaction.commandName === "spinoffs") {
-			interaction.reply("Bot /spinoffs response!");
+			interaction.reply("...");
+			getLiveTags().then(() => {
+				let msg = "";
+				if (!liveTags.length) msg = "terriverse is sleep";
+				liveTags.forEach((tag) => {
+					msg += `[${formatTag(
+						tag
+					)}](<https://tiktok.com/${tag}/live>) `;
+				});
+				interaction.channel?.send(msg);
+			});
 		}
 	}
 );
